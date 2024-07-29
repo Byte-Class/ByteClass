9673c359d1faf4005f6e7d357c611cc7dd3799b8
Dhruv Rayat <dhruvrayat50@gmail.com>
2024-07-29 19:00:10 +1200

    feat: added another env variable, so I just added it to the global process.env

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,6 +5,8 @@ declare global {
       AUTH_GOOGLE_ID: string;
       AUTH_GOOGLE_SECRET: string;
       CONNECTION_URL_POSTGRES: string;
+      GOOGLE_APPLICATION_CREDENTIALS: string;
+      GOOGLE_CALLBACK: string;
     }
   }
 }
