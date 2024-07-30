6d27f69e68e5c4aedf29d8e218e0c657e0a1f1dc
Dhruv Rayat <dhruvrayat50@gmail.com>
2024-07-30 19:14:18 +1200

    feat: home page and dashboard distinction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,34 @@
+import { google } from "googleapis";
 import { auth, signOut } from "auth";
-import { Button } from "@/components/ui/button";
 
 import Navbar from "@/components/navbar";
 import Splash from "@/components/splash";
 import Testimonial from "@/components/testimonial";
+import Line from "@/components/line";
+import Footer from "@/components/footer";
+
+import Dashboard from "@/components/dashboard/dashboard";
 
 export default async function Home() {
   const session = await auth();
 
   console.log(session);
+
+  if (session) {
+    const oauth2Client = new google.auth.OAuth2(
+      process.env.AUTH_GOOGLE_ID,
+      process.env.AUTH_GOOGLE_SECRET,
+      process.env.GOOGLE_CALLBACK,
+    );
+    oauth2Client.setCredentials({
+      // @ts-ignore
+      access_token: session.access_token,
+    });
+    const classroom = google.classroom("v1");
+    google.options({
+      auth: oauth2Client,
+    });
+  }
 
   return (
     <>
@@ -16,12 +36,11 @@ export default async function Home() {
         <>
           <Navbar />
           <Splash />
-          <main className="w-full">
-            <div className="w-4/5 ml-[10%] mr-[10%]">
-              <h2>Testimonials</h2>
-              {/* Hello world */}
+          <main className="flex w-full flex-col gap-20 pb-8 pt-8">
+            <div className="ml-[10%] mr-[10%] w-4/5">
+              <h2 className="text-center text-5xl font-bold">Testimonials</h2>
 
-              <div className="w-full flex items-center justify-between gap-16">
+              <div className="mt-20 flex w-full items-center justify-between gap-16">
                 <Testimonial
                   name="Siddhesh Zantye"
                   quote="This app changed my life."
@@ -39,18 +58,67 @@ export default async function Home() {
                 />
               </div>
             </div>
+
+            <Line />
+
+            <div className="ml-[10%] mr-[10%] flex w-4/5 items-center">
+              <h2 className="flex-1 text-left text-5xl font-bold">
+                Who Are We?
+              </h2>
+              <p className="flex-[3]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum consectetur varius nunc nec ornare. Aliquam at diam
+                justo. Aliquam leo ante, elementum id auctor at, porttitor a
+                ante. Nullam ultrices congue ligula. Maecenas tincidunt nec
+                risus eget euismod. Morbi ac ligula arcu. Phasellus euismod elit
+                vel libero tincidunt sollicitudin. Fusce egestas dolor ut eros
+                condimentum, id sagittis magna finibus. Donec luctus fringilla
+                mauris quis dignissim. Ut iaculis augue diam, eget sollicitudin
+                libero ullamcorper eget. Ut libero augue, hendrerit eu sem a,
+                venenatis ornare justo. Etiam euismod dignissim nunc nec
+                blandit. Praesent imperdiet non nisi interdum porttitor. Aliquam
+                varius ante eget.
+              </p>
+            </div>
+
+            <Line />
+
+            <div className="ml-[10%] mr-[10%] w-4/5 text-center">
+              <h2 className="text-5xl font-bold">Our Mission</h2>
+              <p className="mt-8">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a
+                elit quis nulla venenatis mattis sed sit amet ex. In quis libero
+                tristique, rhoncus est a, auctor tortor. Suspendisse egestas
+                enim nec gravida ultrices. Phasellus ornare sem vitae enim
+                accumsan, eu varius turpis varius. Nam risus neque, ornare vitae
+                mi sit amet, fermentum vestibulum nisl. Curabitur dignissim ac
+                sem ac eleifend. Nulla pharetra, enim at ullamcorper egestas,
+                nisi purus mollis metus, a laoreet massa risus eu massa. Nam
+                velit nibh, aliquet non sollicitudin et, vehicula eu magna. Sed
+                non pharetra augue. Integer malesuada placerat metus ac gravida.
+                Proin laoreet libero vitae pellentesque viverra. Nulla facilisi.
+                Aliquam luctus tincidunt turpis, non accumsan nulla euismod a.
+              </p>
+              <p className="mt-4">
+                Curabitur at tristique ex, in efficitur purus. Aliquam quis
+                fringilla enim. In at arcu at quam rutrum aliquam quis sit amet
+                massa. Praesent vestibulum nisi quam, eu fringilla libero
+                pulvinar sagittis. Nulla facilisi. Nullam fermentum nulla nec
+                augue viverra, et faucibus metus euismod. Sed pulvinar egestas
+                risus, consectetur interdum purus imperdiet id. Nam volutpat,
+                ligula et lacinia scelerisque, lacus magna mattis metus, nec
+                gravida lorem mauris vel tortor. Vestibulum rhoncus massa orci,
+                non vehicula quam eleifend non. Praesent sed nulla dolor. Aenean
+                sit amet leo vel.
+              </p>
+            </div>
+
+            <Footer />
           </main>
         </>
       ) : (
         <>
-          <form
-            action={async () => {
-              "use server";
-              await signOut();
-            }}
-          >
-            <Button type="submit">Sign Out</Button>
-          </form>
+          <Dashboard />
         </>
       )}
     </>
