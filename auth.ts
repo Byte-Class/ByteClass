557a65217c99435a0bca461c1e86e572ee7cd16d
Dhruv Rayat <dhruvrayat50@gmail.com>
2024-07-30 19:15:18 +1200

    !feat: changed provider config, scopes for google auth, aswell as added callbacks, and added custom signin page

--- auth.ts
+++ auth.ts
@@ -1,22 +1,50 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
-import { DrizzleAdapter } from "@auth/drizzle-adapter";
-import { db } from "@/db/db";
-import {
-  users,
-  sessions,
-  accounts,
-  authenticators,
-  verificationTokens,
-} from "@/db/scheme";
+
+import { ROLE_TYPES } from "@/core/types/roles";
+
+const SCOPES = [
+  // General Email and Profile Scopes
+  "profile",
+  "email",
+  "openid",
+  // Courses Scope
+  "https://www.googleapis.com/auth/classroom.courses",
+  "https://www.googleapis.com/auth/classroom.coursework.students.readonly",
+  "https://www.googleapis.com/auth/classroom.coursework.me.readonly",
+  "https://www.googleapis.com/auth/classroom.coursework.me",
+];
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
-  adapter: DrizzleAdapter(db, {
-    usersTable: users,
-    sessionsTable: sessions,
-    accountsTable: accounts,
-    authenticatorsTable: authenticators,
-    verificationTokensTable: verificationTokens,
-  }),
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      authorization: {
+        params: {
+          access_type: "offline",
+          prompt: "consent",
+          scope: SCOPES.join(" "),
+        },
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/signin",
+  },
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.access_token = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      // @ts-ignore
+      session.access_token = token.access_token;
+      //@ts-ignore
+      session.role = ROLE_TYPES.USER;
+      return session;
+    },
+  },
 });
