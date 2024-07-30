6e3d7d98c5f40db1f6d64acdcdeb65bfa25d926e
Dhruv Rayat <dhruvrayat50@gmail.com>
2024-07-30 19:19:43 +1200

    feat: line component changed to also include custom styles dependent on the page you are on

--- src/components/line.tsx
+++ src/components/line.tsx
@@ -1,3 +1,7 @@
-export default function Line() {
-  return <hr className="ml-[10%] mr-[10%] h-1 w-4/5 bg-white" />;
+import { cn } from "@/lib/utils";
+
+export default function Line({ className }: { className?: string }) {
+  return (
+    <hr className={cn("ml-[10%] mr-[10%] h-1 w-4/5 bg-white", className)} />
+  );
 }
