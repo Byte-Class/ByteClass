136af7a274cc3948e9a74b44e5e610e4fc05eb61
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-08-02 19:35:45 +1200

    feat: remove drizzle and added makefile

--- src/db/db.ts
+++
@@ -1,6 +0,0 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import * as schema from "./scheme";
-import postgres from "postgres";
-
-const client = postgres(process.env.CONNECTION_URL_POSTGRES);
-export const db = drizzle(client, { schema, logger: true });
