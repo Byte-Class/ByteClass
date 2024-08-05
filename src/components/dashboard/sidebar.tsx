9425cc8db6110ebc11d3e733a6ae1b6a49ea1ef7
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-08-06 07:23:12 +1200

    feat: update sidebar to use new page urls

--- src/components/dashboard/sidebar.tsx
+++ src/components/dashboard/sidebar.tsx
@@ -8,33 +8,21 @@ type Link = {
 
 export default function SideBar() {
   return (
-    <div className="flex h-full min-h-lvh w-80 flex-col gap-2 bg-lightBlack">
-      <Link
-        key={crypto.randomUUID()}
-        className="ml-12 text-2xl font-bold"
-        href={"/calender"}
-      >
-        Calender
+    <div className="flex h-full min-h-[calc(100lvh-7rem)] w-80 flex-col gap-2 bg-lightBlack">
+      <Link className="ml-12 text-2xl font-bold" href={"/dashboard/calendar"}>
+        Calendar
       </Link>
-      <Line key={crypto.randomUUID()} className="ml-auto mr-4 w-3/4" />
+      <Line className="ml-auto mr-4 w-3/4" />
 
-      <Link
-        key={crypto.randomUUID()}
-        className="ml-12 text-2xl font-bold"
-        href={"/myFiles"}
-      >
+      <Link className="ml-12 text-2xl font-bold" href={"/dashboard/myfiles"}>
         My Files
       </Link>
-      <Line key={crypto.randomUUID()} className="ml-auto mr-4 w-3/4" />
+      <Line className="ml-auto mr-4 w-3/4" />
 
-      <Link
-        key={crypto.randomUUID()}
-        className="ml-12 text-2xl font-bold"
-        href={"/tasks"}
-      >
+      <Link className="ml-12 text-2xl font-bold" href={"/dashboard/tasks"}>
         Tasks
       </Link>
-      <Line key={crypto.randomUUID()} className="ml-auto mr-4 w-3/4" />
+      <Line className="ml-auto mr-4 w-3/4" />
     </div>
   );
 }
