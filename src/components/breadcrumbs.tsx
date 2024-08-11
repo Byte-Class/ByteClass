703557072de655eba38a537d07884fdee6470e08
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-08-11 21:23:47 +1200

    feat: laying the breadcrumbs for the ducks :bread: :duck: :duck: :duck

---
+++ src/components/breadcrumbs.tsx
@@ -0,0 +1,39 @@
+// Time to lay the crumbs for THE DUCKS!!!!!
+
+"use client";
+
+import { usePathname } from "next/navigation";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+export default function BreadCrumbs() {
+  // Get's the pathname, but due to some things I can not control, I need to do this :(
+  const pathname = usePathname().split("/").splice(2, 3)[0];
+
+  console.log(pathname);
+
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/dashboard">Home</BreadcrumbLink>
+        </BreadcrumbItem>
+        {pathname && (
+          <>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>{pathname}</BreadcrumbPage>
+            </BreadcrumbItem>
+          </>
+        )}
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
