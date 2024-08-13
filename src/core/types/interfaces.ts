5c931d77b49b646ec3f71220dfdb41f65129c126
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-08-13 17:44:03 +1200

    feat: interfaces for other components

--- src/core/types/interfaces.ts
+++ src/core/types/interfaces.ts
@@ -1,4 +1,11 @@
+import { classroom_v1 } from "googleapis";
+
 export interface CourseList {
   id: string | null | undefined;
   name: string | null | undefined;
+}
+
+export interface PropsSectionTasks {
+  sectionHeader: string;
+  itemsToShow: classroom_v1.Schema$StudentSubmission[];
 }
