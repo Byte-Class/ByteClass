fb8ef4cc43f72ccc12d5c6147faa4022ef3a447f
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-08-15 21:35:06 +1200

    fix: drizzle taking up ALL the postgres connections

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -2,5 +2,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import * as schema from "./schema";
 import postgres from "postgres";
 
-const client = postgres(process.env.DATABASE_URL);
+const client = postgres(process.env.DATABASE_URL, {
+  idle_timeout: 1,
+});
 export const db = drizzle(client, { schema, logger: true });
