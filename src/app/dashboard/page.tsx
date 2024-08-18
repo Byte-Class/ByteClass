18afff04df53323ae046faf03a18fa6ff180644b
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-08-18 20:26:29 +1200

    feat: remove oauth 2 client from component, not need right now

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,25 +1,3 @@
-import { auth } from "auth";
-import { google } from "googleapis";
-
 export default async function Dashboard() {
-  const session = await auth();
-
-  if (session) {
-    const oauth2Client = new google.auth.OAuth2(
-      process.env.AUTH_GOOGLE_ID,
-      process.env.AUTH_GOOGLE_SECRET,
-      process.env.GOOGLE_CALLBACK,
-    );
-    oauth2Client.setCredentials({
-      access_token: session.access_token,
-    });
-
-    const classroom = google.classroom("v1");
-
-    google.options({
-      auth: oauth2Client,
-    });
-  }
-
   return <main>Dashboard</main>;
 }
