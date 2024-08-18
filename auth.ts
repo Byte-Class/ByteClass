9725fd7cd3aed7384290904425d388c7f4838018
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-08-18 20:27:55 +1200

    feat: new database auth instead of custom jwt's

--- auth.ts
+++ auth.ts
@@ -1,12 +1,10 @@
-import jwt from "jsonwebtoken";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
-import type { JWT, JWTDecodeParams } from "next-auth/jwt";
 
 import { db } from "@/drizzle/db";
 import { eq } from "drizzle-orm";
-import { accounts, users } from "@/drizzle/schema";
+import { accounts } from "@/drizzle/schema";
 
 const SCOPES = [
   // General Email and Profile Scopes
@@ -33,6 +31,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           scope: SCOPES.join(" "),
         },
       },
+      account(account) {
+        account.access_token;
+        return account;
+      },
     }),
   ],
   pages: {
@@ -41,94 +43,28 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     newUser: "/onboarding",
   },
   session: {
-    strategy: "jwt",
+    strategy: "database",
   },
   callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.user = user;
-      }
-
-      return token;
-    },
-    async session({ session, token }) {
-      if (token) {
-        session.user = token.user;
-        session.id = token.id;
-      }
-
-      const res = (
+    async session({ session }) {
+      const tokens = (
         await db
           .select({
-            refreshToken: accounts.refresh_token,
             accessToken: accounts.access_token,
+            refreshToken: accounts.refresh_token,
           })
           .from(accounts)
-          .where(eq(accounts.userId, session.id))
+          .where(eq(accounts.userId, session.user.id))
       )[0];
 
-      const picture = (
-        await db
-          .select({
-            picture: users.image,
-          })
-          .from(users)
-          .where(eq(users.id, session.id))
-      )[0];
-
-      // check if access token or refresh token are null, if so we just return the session
-      if (res.accessToken === null || res.refreshToken === null) {
+      if (tokens.accessToken === null || tokens.refreshToken === null) {
         return session;
       }
 
-      if (picture.picture === null) {
-        return session;
-      }
-
-      session.user.picture = picture.picture;
-      session.access_token = res.accessToken;
-      session.refresh_token = res.refreshToken;
+      session.accessToken = tokens.accessToken;
+      session.refreshToken = tokens.refreshToken;
 
       return session;
-    },
-  },
-  secret: process.env.ACCESS_TOKEN_SECRET,
-  jwt: {
-    encode: async ({ token }) => {
-      if (!token) {
-        return "";
-      }
-
-      const data = await db
-        .select({
-          id: users.id,
-        })
-        .from(users)
-        .where(eq(users.email, token.user.email));
-
-      const newToken = jwt.sign(
-        {
-          id: data[0].id,
-          user: {
-            name: token.user.name,
-            email: token.user.email,
-            picture: token.user.picture,
-          },
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        {
-          expiresIn: 7 * 24 * 60 * 60, // 7 days
-        },
-      );
-
-      return newToken;
-    },
-    decode: async ({ token }: JWTDecodeParams): Promise<JWT | null> => {
-      const decodedToken = jwt.verify(
-        token as string,
-        process.env.ACCESS_TOKEN_SECRET,
-      ) as JWT;
-      return decodedToken;
     },
   },
 });
