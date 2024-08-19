006e75c84fb4091ba751d4a135333ca3eb558e5e
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-08-20 09:59:13 +1200

    feat: added widgets to dashvioar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,16 @@
+import Agenda from "@/components/dashboard/agenda";
+import DueSoon from "@/components/dashboard/due-soon";
+import ToDo from "@/components/dashboard/todo";
+
 export default async function Dashboard() {
-  return <main>Dashboard</main>;
+  return (
+    <main className="w-full">
+      <div className="flex w-full gap-4">
+        <DueSoon />
+        <ToDo />
+      </div>
+
+      <Agenda />
+    </main>
+  );
 }
