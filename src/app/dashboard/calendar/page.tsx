38168dee1b513f9480dd7d740c7af51ebcf9b44b
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-08-20 09:59:25 +1200

    feat: calendar page updated

--- src/app/dashboard/calendar/page.tsx
+++ src/app/dashboard/calendar/page.tsx
@@ -1,15 +1,22 @@
-import TestComponent from "@/components/test";
 import { auth } from "auth";
-import { google } from "googleapis";
+import { Metadata } from "next";
 import { SessionProvider } from "next-auth/react";
+
+import CalendarWeekSwitcher from "@/components/calendar/entire-calendar/switcher";
+import CalendarWeek from "@/components/calendar/entire-calendar/type/week";
+
+export const metadata: Metadata = {
+  title: "Calendar | Byte Class",
+};
 
 export default async function Calendar() {
   const session = await auth();
 
   return (
-    <main>
+    <main className="min-h-lvh w-full">
       <SessionProvider session={session}>
-        <TestComponent />
+        <CalendarWeekSwitcher />
+        <CalendarWeek />
       </SessionProvider>
     </main>
   );
