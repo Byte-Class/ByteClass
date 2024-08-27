4eae6ee4ca1b12142a2b7f11f85d6508667e1fae
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-08-27 20:35:21 +1200

    feat: changed button to be from only create event, to now also including a create calendar button

--- src/components/calendar/sidebar.tsx
+++ src/components/calendar/sidebar.tsx
@@ -1,26 +1,59 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCalendarDay,
+  faCalendarDays,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
+import { useSetAtom } from "jotai";
 
-import { Button } from "@/components/ui/button";
+import { ATOM_CREATE_EVENT_MODEL } from "@/core/atoms/atom";
+
 import Line from "@/components/line";
 import ChoseCalendar from "@/components/calendar/chose-calendar/chose-calendar";
-import { useSetAtom } from "jotai";
-import { ATOM_EVENT_MODAL } from "@/core/atoms/atom";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function SideBarCalendar() {
-  const setModal = useSetAtom(ATOM_EVENT_MODAL);
+  const setEventModal = useSetAtom(ATOM_CREATE_EVENT_MODEL);
 
   return (
     <div className="flex h-full min-h-[calc(100lvh-7rem)] w-80 flex-col bg-lightBlack p-4">
-      <Button
-        className="flex gap-2"
-        onClick={() => {
-          setModal(true);
-        }}
-      >
-        <FontAwesomeIcon icon={faPlus} />
-        Create Event
-      </Button>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button className="flex justify-start gap-4">
+            <FontAwesomeIcon icon={faPlus} />
+            Create
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-60">
+          <DropdownMenuLabel>Creations</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem onToggle={() => alert("Create Calendar")}>
+              <FontAwesomeIcon icon={faCalendarDays} className="mr-2" />
+              <span>Calendar</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onSelect={() => {
+                setTimeout(() => {
+                  setEventModal(true);
+                }, 150);
+              }}
+            >
+              <FontAwesomeIcon icon={faCalendarDay} className="mr-2" />
+              <span>Event</span>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
 
       <div className="mt-auto">
         <h2 className="text-2xl font-bold">Calendars</h2>
