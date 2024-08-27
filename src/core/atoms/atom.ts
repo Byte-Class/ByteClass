673d4f1ec76086fccc50ab0e70fdc0d298604a80
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-08-27 14:19:55 +1200

    feat: atom for current calendars, so I don't need to fetch them twice

--- src/core/atoms/atom.ts
+++ src/core/atoms/atom.ts
@@ -1,7 +1,10 @@
 import { atom } from "jotai";
+import { Calendar } from "../types/interfaces";
 
 export const ATOM_CALENDAR_TYPE = atom<"month" | "week" | "day">("week");
 
 export const ATOM_CURRENT_DAY = atom<Date>(new Date());
 
 export const ATOM_EVENT_MODAL = atom<boolean>(false);
+
+export const ATOM_CALENDARS = atom<Calendar[] | undefined>(undefined);
