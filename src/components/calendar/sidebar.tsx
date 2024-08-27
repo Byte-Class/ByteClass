c515cc641e2c0a0ea9def7d6f3b155003e8f1def
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-08-27 14:22:10 +1200

    feat: atoms for opening modal

--- src/components/calendar/sidebar.tsx
+++ src/components/calendar/sidebar.tsx
@@ -4,19 +4,18 @@ import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { Button } from "@/components/ui/button";
 import Line from "@/components/line";
 import ChoseCalendar from "@/components/calendar/chose-calendar/chose-calendar";
-import { useAtomValue, useSetAtom } from "jotai";
+import { useSetAtom } from "jotai";
 import { ATOM_EVENT_MODAL } from "@/core/atoms/atom";
 
 export default function SideBarCalendar() {
   const setModal = useSetAtom(ATOM_EVENT_MODAL);
-  const getModal = useAtomValue(ATOM_EVENT_MODAL);
 
   return (
     <div className="flex h-full min-h-[calc(100lvh-7rem)] w-80 flex-col bg-lightBlack p-4">
       <Button
         className="flex gap-2"
         onClick={() => {
-          setModal(!getModal);
+          setModal(true);
         }}
       >
         <FontAwesomeIcon icon={faPlus} />
