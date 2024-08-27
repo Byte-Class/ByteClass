a5307a69f3c0e801fc8920b5d4aad4dd5322c644
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-08-27 20:35:45 +1200

    feat: atom for create calendar modal

--- src/core/atoms/atom.ts
+++ src/core/atoms/atom.ts
@@ -5,6 +5,8 @@ export const ATOM_CALENDAR_TYPE = atom<"month" | "week" | "day">("week");
 
 export const ATOM_CURRENT_DAY = atom<Date>(new Date());
 
-export const ATOM_EVENT_MODAL = atom<boolean>(false);
+export const ATOM_CREATE_EVENT_MODEL = atom<boolean>(false);
+
+export const ATOM_CREATE_CALENDAR_MODEL = atom<boolean>(false);
 
 export const ATOM_CALENDARS = atom<Calendar[] | undefined>(undefined);
