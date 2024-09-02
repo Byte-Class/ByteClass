9eb5d3e2a643a3c2f48a084462218734dc0d8550
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-09-03 08:37:31 +1200

    feat: trpc vanilla sevrer added

---
+++ src/server/trpc.ts
@@ -0,0 +1,27 @@
+import { initTRPC, TRPCError } from "@trpc/server";
+import { Context } from "./context";
+
+const t = initTRPC.context<Context>().create();
+
+export const router = t.router;
+export const procedure = t.procedure;
+
+export const adminProcedure = t.procedure.use(({ ctx, next }) => {
+  console.log(ctx.session);
+
+  if (!ctx.session) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      cause: "MAYBE TRY GETTING A FUCKING SESSION",
+      message: "Bro you are NOT authorized",
+    });
+  }
+  return next({
+    ctx: {
+      user: {
+        id: (ctx.session.user as any).id,
+        sessionToken: (ctx.session.user as any).id,
+      },
+    },
+  });
+});
