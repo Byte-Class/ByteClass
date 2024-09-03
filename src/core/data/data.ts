90a0c31a3d8c24216879861351f934b7b02b41e7
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-09-03 14:50:46 +1200

    feat: data for colours type

---
+++ src/core/data/data.ts
@@ -0,0 +1,8 @@
+export const COLOURS = [
+  { label: "Red", value: "F02D3A" },
+  { label: "Orange", value: "FF964F" },
+  { label: "Yellow", value: "F0D975" },
+  { label: "Green", value: "57BD57" },
+  { label: "Blue", value: "6FA8D6" },
+  { label: "Purple", value: "A185D6" },
+] as const;
