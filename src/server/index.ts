32a73c3c3d061573ba7c0ad30771b77ea3ddfbc6
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-09-03 14:51:44 +1200

    feat: added event router to server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,7 @@
 import { router } from "./trpc";
 import { calendar } from "./routers/calendar";
+import { events } from "./routers/event";
 
-export const appRouter = router({ calendar });
+export const appRouter = router({ calendar, events });
 
 export type AppRouter = typeof appRouter;
