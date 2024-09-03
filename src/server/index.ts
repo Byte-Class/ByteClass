933c4302af83ef5e1f86b6be9fcaedfaceb67ef9
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-09-03 20:39:44 +1200

    feat: added tasks route to trpc

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,8 @@
 import { router } from "./trpc";
 import { calendar } from "./routers/calendar";
 import { events } from "./routers/event";
+import { tasks } from "./routers/tasks";
 
-export const appRouter = router({ calendar, events });
+export const appRouter = router({ calendar, events, tasks });
 
 export type AppRouter = typeof appRouter;
