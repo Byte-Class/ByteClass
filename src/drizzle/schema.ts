a87d4a95985ac72ba72b00b87af8295155fc09e6
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-09-03 15:06:24 +1200

    feat: drizzle migrations

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -65,27 +65,6 @@ export const verificationTokens = pgTable(
   }),
 );
 
-export const authenticators = pgTable(
-  "authenticator",
-  {
-    credentialID: text("credentialID").notNull().unique(),
-    userId: text("userId")
-      .notNull()
-      .references(() => users.id, { onDelete: "cascade" }),
-    providerAccountId: text("providerAccountId").notNull(),
-    credentialPublicKey: text("credentialPublicKey").notNull(),
-    counter: integer("counter").notNull(),
-    credentialDeviceType: text("credentialDeviceType").notNull(),
-    credentialBackedUp: boolean("credentialBackedUp").notNull(),
-    transports: text("transports"),
-  },
-  (authenticator) => ({
-    compositePK: primaryKey({
-      columns: [authenticator.userId, authenticator.credentialID],
-    }),
-  }),
-);
-
 export const kamar = pgTable("kamar", {
   userId: text("userId")
     .notNull()
