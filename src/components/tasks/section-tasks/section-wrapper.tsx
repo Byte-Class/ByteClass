de4f185e2e7a73067bdcefc298517f2bf72280f1
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-09-07 09:50:52 +1200

    feat: added overdue section to wrapper

--- src/components/tasks/section-tasks/section-wrapper.tsx
+++ src/components/tasks/section-tasks/section-wrapper.tsx
@@ -1,45 +1,7 @@
-import { google } from "googleapis";
-import { auth } from "auth";
-import { accounts, activeCourses } from "tables";
-import { db } from "db";
-import { and, eq } from "drizzle-orm";
-import { toast } from "react-toastify";
-
-import { authGoogle } from "@/core/utils/auth-google";
-
-import SectionTasks from "@/components/tasks/section-tasks/section-tasks";
-// import { FetchGoogle } from "@/core/utils/fetch-google";
-import { trpc } from "@/server/client";
+import OverdueSection from "./overdue-section";
 import HandedInSection from "./handed-in-section";
 
 export default async function SectionTasksWrapper() {
-  const session = await auth();
-
-  if (!session || !session.user || !session.user.id) {
-    return toast.error("You are not authorized");
-  }
-
-  // request data from database
-  const { refresh_token } = (
-    await db.select().from(accounts).where(eq(accounts.userId, session.user.id))
-  )[0];
-
-  const active = (
-    await db
-      .select({
-        courseId: activeCourses.courseId,
-      })
-      .from(activeCourses)
-      .where(
-        and(
-          eq(activeCourses.userId, session.user.id),
-          eq(activeCourses.active, true),
-        ),
-      )
-  ).map((course) => course.courseId);
-
-  authGoogle(refresh_token);
-
   // const courseWorksOverdue = await new FetchGoogle(active).courseWorksOverDue();
 
   // const returnedReclaimedReturned = await new FetchGoogle(
@@ -54,6 +16,7 @@ export default async function SectionTasksWrapper() {
         sectionHeader="Returned"
         itemsToShow={returnedReclaimedReturned}
       /> */}
+      <OverdueSection />
       <HandedInSection />
     </>
   );
